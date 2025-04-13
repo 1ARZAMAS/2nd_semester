fbc6ced49cddf920c37bc07caf7cde5e989f6a37
Елизавета <131687570+1ARZAMAS@users.noreply.github.com>
2025-04-13 17:44:00 +0700

    Delete RGR directory

--- RGR/Header1.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-void viginer(std::string& password);
-void rsa(std::string& password);
-void scytale(std::string& password);
-void writeToFile(const std::string& filename, const std::string& content);
-std::string readFromFile(const std::string& filename);
